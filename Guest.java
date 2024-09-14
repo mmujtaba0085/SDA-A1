6cacf1c4635e5faa6ec4624f07df57abb8739bff
Mohammad Mujtaba <mmujtaba0085@gmail.com>
2024-09-15 03:23:41 +0500

    Basic Guest Functionality Added

---
+++ Guest.java
@@ -0,0 +1,87 @@
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class Guest {
+    protected int guestID;
+    protected String Name;
+    protected String Email;
+    protected List<String> BookingHistory;
+    protected double TotalFee;
+    protected String phoneNumber;
+    protected String Address;
+    protected String GuestType;
+    protected int TotalEntries;  //How many times he has visited the hotel
+    private static int totalGuest=0;
+
+    Guest(String Name,String Email,String phoneNumber,String Address,String GuestType){
+        this.guestID=++totalGuest;
+        this.Name=Name;
+        this.Email=Email;
+        this.BookingHistory= new ArrayList<>();
+        this.TotalFee=0;
+        this.phoneNumber=phoneNumber;
+        this.Address=Address;
+        this.GuestType=GuestType;
+    }
+
+    Guest(String Name,String Email,String phoneNumber,String Address,String GuestType,int GuestID){  //For Transfering Customer from 
+        this.guestID=GuestID;                                                                        //one child class to another (i.e Regular->Frequent)
+        this.Name=Name;
+        this.Email=Email;
+        this.BookingHistory=null;
+        this.TotalFee=0;
+        this.phoneNumber=phoneNumber;
+        this.Address=Address;
+        this.GuestType=GuestType;
+    }
+    public void PrintInfo(){
+        System.out.println("Guest ID: " + guestID);
+        System.out.println("Name: " + Name);
+        System.out.println("Email: " + Email);
+        System.out.println("Phone: " + phoneNumber);
+        System.out.println("Address: " + Address);
+        System.out.println("Guest Type: " + GuestType);
+        System.out.println("Total Fee: " + TotalFee);
+        System.out.println("Total Entries: " + TotalEntries);
+        System.out.println("Booking History: " + BookingHistory);
+    }
+}
+
+
+class Regular extends Guest{
+
+    Regular(String Name,String Email,String phoneNumber,String Address){
+        super(Name, Email, phoneNumber, Address,"Regular");
+    }
+
+    Frequent FrequentGuestCheck(){
+        if(TotalEntries>=3) //if total visitation is more or equal to 3 he is a frequent Guest
+        {
+            return new Frequent(this);
+        }
+        return null;
+    }
+}
+
+
+class Frequent extends Guest{
+
+    Frequent(String Name,String Email,String phoneNumber,String Address){
+        super(Name, Email, phoneNumber, Address,"Frequent");
+    }
+
+    Frequent(Regular guest){    
+        super(guest.Name, guest.Email, guest.phoneNumber, guest.Address,"Frequent",guest.guestID);
+        this.BookingHistory=guest.BookingHistory;
+        this.TotalEntries=guest.TotalEntries;
+        this.TotalFee=guest.TotalFee;
+    }
+}
+
+class Corporate extends Guest{
+
+    Corporate(String Name,String Email,String phoneNumber,String Address){
+        super(Name, Email, phoneNumber, Address,"Corporate");
+    }
+
+}
--- MainMenu.java
+++ MainMenu.java
@@ -1,10 +1,22 @@
 public class MainMenu{
     public static void main(String args[]){
-        SingleRoom room1 = new SingleRoom(101, 15000, "Wifi,AC");
-        room1.PrintInfo();
-        DoubleRoom room2 = new DoubleRoom(102,25000, "Wifi, AC");
-        room2.PrintInfo();
-        Suite room3 = new Suite(103,35000, "Wifi, AC");
-        room3.PrintInfo();
+        // SingleRoom room1 = new SingleRoom(101, 15000, "Wifi,AC");
+        // DoubleRoom room2 = new DoubleRoom(102,25000, "Wifi, AC");
+        // Suite room3 = new Suite(103,35000, "Wifi, AC");
+
+        // room1.PrintInfo();
+        // room2.PrintInfo();
+        // room3.PrintInfo();
+
+        // Regular guest1 = new Regular("Shawn Mend","blahblah123@gmail.com","03331306001","street3,Hyderabad");
+        // Frequent guest2 = new Frequent("Shawn eed","blahblah124@gmail.com","03331306002","street6,Hyderabad");
+        // Corporate guest3 = new Corporate("hawn eed","blahssblah124@gmail.com","03321306002","street8,Hyderabad");
+        
+        // guest1.PrintInfo();
+        // guest2.PrintInfo();
+        // guest3.PrintInfo();
+
+        // Frequent guestChange = new Frequent(guest1);
+        // guestChange.PrintInfo();
     }
 }
